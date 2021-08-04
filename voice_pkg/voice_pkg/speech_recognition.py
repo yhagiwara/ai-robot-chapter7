a4271aa5cc1aa195617d199a32d2d913b5440baf
Masaki Ito <ito.masaki@em.ci.ritsumei.ac.jp>
2021-08-04 13:09:28 +0900

    [WIP] add speech recognition program in real time

--- voice_pkg/voice_pkg/speech_recognition.py
+++ voice_pkg/voice_pkg/speech_recognition.py
@@ -3,6 +3,8 @@ import rclpy.node
 from std_msgs.msg import String
 
 from time import sleep
+import speech_recognition as sr
+import pyaudio
 
 class SpeechRecognition(rclpy.node.Node):
     def __init__(self):
@@ -11,21 +13,32 @@ class SpeechRecognition(rclpy.node.Node):
         self.logger = self.get_logger()
         self.logger.info("Start speech recognition")
 
+        self.period = 5.0
+        self.init_rec = sr.Recognizer()
+
         self.recognized_pub = self.create_publisher(String, "recognized_text", 10)
 
+        self.timer = self.create_timer(self.period, self.recognition)
+
         # [TODO]
-        #
         # 音声認識を行うプログラムを作成
 
-        sleep(5)
-        self.test("Bring me a bottle from dining")
+    def recognition(self):
+        with sr.Microphone() as source:
+            audio_data = self.init_rec.record(source, duration=5)
+            print("Recognizing your speech.......")
 
-    def test(self, text):
+            try:
+                text = self.init_rec.recognize_google(audio_data)
+                print(text)
+
+            except sr.UnknownValueError:
+                pass
+
         msg = String()
-        msg.data = text
-
+        msg.data = "Bring me a bottle from dining"
         self.recognized_pub.publish(msg)
-        self.logger.info("Published recognized text '{}'".format(text))
+        self.logger.info("Published recognized text '{}'".format(msg.data))
 
 
 def main():
@@ -33,9 +46,7 @@ def main():
 
     speech_recognition = SpeechRecognition()
 
-    try:
-        rclpy.spin(speech_recognition)
-    except:
-        speech_recognition.destroy_node()
+    rclpy.spin(speech_recognition)
+    speech_recognition.destroy_node()
 
     rclpy.shutdown()
